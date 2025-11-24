5988da2b849d9ed965ae137ce5a40f074c9d04e1
SpydyWeb <70287710+SpydyWeb@users.noreply.github.com>
2025-11-24 17:59:03 +0530

    sldk

--- Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c966f46366e7c9cf866a9ef55255f8873e56ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6090de06bd0d631191e539463131c0abd3240788")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
